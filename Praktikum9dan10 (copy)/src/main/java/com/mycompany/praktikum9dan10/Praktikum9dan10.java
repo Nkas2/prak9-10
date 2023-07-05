06fc8f2467c9653bc77ca9ad13492695e12d487f
Nay <103982970+Nkas2@users.noreply.github.com>
2023-07-05 08:50:25 +0700

    Add files via upload

---
+++ Praktikum9dan10 (copy)/src/main/java/com/mycompany/praktikum9dan10/Praktikum9dan10.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.praktikum9dan10;
+
+/**
+ *
+ * @author nayandra
+ */
+public class Praktikum9dan10 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
